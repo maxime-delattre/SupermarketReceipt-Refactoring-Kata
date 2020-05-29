6e5a5c7f130555cea2f268a9e5dadc19c3a2d9bb
jebo <jbouhi@octo.com>
2020-05-29 14:50:41 +0200

    1st test

--- typescript/test/Supermarket.test.ts
+++ typescript/test/Supermarket.test.ts
@@ -1,11 +1,12 @@
+import { ReceiptPrinter } from '../src/ReceiptPrinter'
 import {FakeCatalog} from "./FakeCatalog"
 import {Product} from "../src/model/Product"
 import {SupermarketCatalog} from "../src/model/SupermarketCatalog"
 import {Receipt} from "../src/model/Receipt"
 import {ShoppingCart} from "../src/model/ShoppingCart"
 import {Teller} from "../src/model/Teller"
-import {SpecialOfferType} from "../src/model/SpecialOfferType"
 import {ProductUnit} from "../src/model/ProductUnit"
+import { expect } from 'chai'
 const approvals = require('approvals')
 
 type Approvals = {
@@ -15,24 +16,16 @@ type Approvals = {
 describe('Supermarket', function () {
 
     approvals.mocha()
-    it('TODO decide what to specify', function (this: any) {
+    it('for 1 item without any special offer, it prints the right receipt', function (this: any) {
+        const toothbrush = new Product('toothbrush', ProductUnit.Each)
 
-        const catalog: SupermarketCatalog = new FakeCatalog();
-        const toothbrush: Product = new Product("toothbrush", ProductUnit.Each);
-        catalog.addProduct(toothbrush, 0.99);
-        const apples: Product = new Product("apples", ProductUnit.Kilo);
-        catalog.addProduct(apples, 1.99);
+        const receipt = new Receipt()
+        receipt.addProduct(toothbrush, 1, 0.99, 0.99)
 
-        const cart: ShoppingCart = new ShoppingCart();
-        cart.addItemQuantity(apples, 2.5);
+        const receiptPrinter = new ReceiptPrinter()
+        const printedReceipt = receiptPrinter.printReceipt(receipt)
 
-        const teller: Teller = new Teller(catalog);
-        teller.addSpecialOffer(SpecialOfferType.TenPercentDiscount, toothbrush, 10.0);
-
-        const receipt: Receipt = teller.checksOutArticlesFrom(cart);
-
-        // Todo: complete this test
-        this.verifyAsJSON({})
+        expect(printedReceipt).to.eq('toothbrush                          0.99\n\nTotal:                              0.99')
     });
 
 });
