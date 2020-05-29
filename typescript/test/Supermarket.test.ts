905d4cc38ce4d37e59ea298eaa45f5addaaa2ee3
maxime <deme@octo.com>
2020-05-29 15:04:11 +0200

    wip

--- typescript/test/Supermarket.test.ts
+++ typescript/test/Supermarket.test.ts
@@ -7,6 +7,7 @@ import {ShoppingCart} from "../src/model/ShoppingCart"
 import {Teller} from "../src/model/Teller"
 import {ProductUnit} from "../src/model/ProductUnit"
 import { expect } from 'chai'
+import {Discount} from "../src/model/Discount";
 const approvals = require('approvals')
 
 type Approvals = {
@@ -27,5 +28,46 @@ describe('Supermarket', function () {
 
         expect(printedReceipt).to.eq('toothbrush                          0.99\n\nTotal:                              0.99')
     });
+
+    it('for 2 differents items without any special offer, it prints the right receipt', function (this: any) {
+        const toothbrush = new Product('toothbrush', ProductUnit.Each)
+        const soap = new Product('soap', ProductUnit.Each)
+
+        const receipt = new Receipt()
+        receipt.addProduct(toothbrush, 1, 0.99, 0.99)
+        receipt.addProduct(soap, 1, 3.99, 3.99)
+
+        const receiptPrinter = new ReceiptPrinter()
+        const printedReceipt = receiptPrinter.printReceipt(receipt)
+
+        expect(printedReceipt).to.eq('toothbrush                          0.99\nsoap                                3.99\n\nTotal:                              4.98')
+    });
+
+    it('for 2 items without any special offer, it prints the right receipt', function (this: any) {
+        const soap = new Product('soap', ProductUnit.Each)
+
+        const receipt = new Receipt()
+        receipt.addProduct(soap, 2, 3.99, 7.98)
+
+        const receiptPrinter = new ReceiptPrinter()
+        const printedReceipt = receiptPrinter.printReceipt(receipt)
+
+        expect(printedReceipt).to.eq('soap                                7.98\n  3.99 * 2\n\nTotal:                              7.98')
+    });
+
+    it('for 1 item with special discount, it prints the right receipt', function(this: any) {
+        const soap = new Product('soap', ProductUnit.Each)
+        const discount = new Discount(soap, 'Special offer -10%', 0.40)
+
+        const receipt = new Receipt()
+        receipt.addProduct(soap, 1, 3.99, 3.99)
+        receipt.addDiscount(discount)
+
+        const receiptPrinter = new ReceiptPrinter()
+        const printedReceipt = receiptPrinter.printReceipt(receipt)
+
+        expect(printedReceipt).to.eq('soap                                3.99\nSpecial offer -10%(soap)           -0.40\n\nTotal:                              3.59')
+
+    })
 
 });
